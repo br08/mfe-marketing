1147a42674c126ccd3f9517bbcf5c08d9f5549fd
br08 <brayan.neves@gmail.com>
2023-03-30 19:25:34 -0300

    added the Landing and Pricing and added them to App with routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 export default () => {
   return (
     <div>
-      <h1>Hello there!</h1>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Landing}></Route>
+            <Route exact path="/pricing" component={Pricing}></Route>
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
   );
 };
