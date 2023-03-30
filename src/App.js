329bedfff49a910a1868bd74d262492f9303c908
br08 <brayan.neves@gmail.com>
2023-03-30 19:18:49 -0300

    added App as the default initial component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello there!</h1>
+    </div>
+  );
+};
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import App from "./App";
+
 // Mount function to start up the app.
 const mount = (el) => {
-  ReactDOM.render(<h1>Hello there!</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 
 // If we are in development and in isolation, call mount immediately.
